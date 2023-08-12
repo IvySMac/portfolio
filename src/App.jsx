fadb219b407720bce6e27ecfa08b9ea4e91a021d
Creature Feature <ivy.mccurdy@gmail.com>
2023-08-11 19:02:28 -0500

    created boilerplate for portfolio, began adding Navbar and Main body

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="bucket-app">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
