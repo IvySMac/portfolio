79dce8579ac539ddb99a12d34efd117fb6254119
Creature Feature <ivy.mccurdy@gmail.com>
2023-08-13 23:59:47 -0500

    began to add routing between pages, but deleted it. Will try something different

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import AboutMe from './pages/AboutMe'
-
+/*Pages*/
+// import Home from './pages/Home'
+// import Projects from './pages/Projects'
+// import AboutMe from './pages/AboutMe'
+/*Components*/
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 function App() {
   const [displayState, setDisplayState] = useState("");
-
-  const DisplaySwitch = (state) =>{
-    switch (state) {
-      case "": 
-        return <Home />;
-      case "projects":
-        return <Projects />;
-      case "about":
-        return <AboutMe />;
-    };
-  }
 
   return (
     <>
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
 export default function Navbar (){
 
     return (
-        <div>Navbar</div>
+        <div>
+            <a></a>
+            <a></a>
+            <a></a>
+        </div>
     )
 }
