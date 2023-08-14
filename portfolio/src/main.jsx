82f91dd41c29f4d8c0a358459a4f2a20fa8f1329
Creature Feature <ivy.mccurdy@gmail.com>
2023-08-13 22:05:19 -0500

    completely deleted boiler plate code from mini project activity, started by scratch with a vite build, tried to pull files out of folder but didn't succeed. re-did that too. Added info to README and background in index.css

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,14 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const router = createBrowserRouter ([
-  {
-    path: 
-    
-  }
-]) 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
--- src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
