2ad3ff709d290455be9fffd5d68774c4bd05b9ee
Creature Feature <ivy.mccurdy@gmail.com>
2023-08-19 04:38:55 -0500

    added a responsive design so it looks okay on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <img src="/logo.png" alt="Ivy McCurdy" />
+      <img className="ivy" src="/logo.png" alt="Ivy McCurdy" />
     </div>
   );
 }
